deb5e065e72a617d8f0ea2fc364a7c439254228b
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-04 16:48:53 -0500

    Added prompts for the user to enter in words and outputs short story/poem

--- MadLibs/Program.cs
+++ MadLibs/Program.cs
@@ -10,6 +10,20 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
+            string color, pluralNoun, celebrity;
+
+            Console.WriteLine("Enter a color: ");
+            color = Console.ReadLine();
+            Console.WriteLine("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+            Console.WriteLine("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are {0}", color);
+            Console.WriteLine("{0} are blue", pluralNoun);
+            Console.WriteLine("I love {0}", celebrity);
+
+            Console.ReadLine();
         }
     }
 }
